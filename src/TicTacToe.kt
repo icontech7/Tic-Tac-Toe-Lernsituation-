e839f9012610920dca20cb1fa9372a3660496936
ahmad <graffiti4kin@gmail.com>
2018-12-14 11:18:49 +0100

    finishing the Cod

--- src/TicTacToe.kt
+++ src/TicTacToe.kt
@@ -1,9 +1,18 @@
 import java.util.*
 
-var board = arrayOfNulls<String>(9)
-private var gameField: String? = null
-val gameSituation = arrayListOf<String>(
-        "X", "", "",
+
+/**
+ * Lernsituation Tic-Tac-toe
+ *
+ * @author Ahmad afa alrefaey
+ */
+
+
+private var board = arrayOfNulls<String>(9)
+private var positive = arrayListOf<String>()
+private var negative = arrayListOf<String>()
+private val gameSituation = arrayListOf<String>(
+        "X", "O", "O",
         "O", "X", "X",
         "X", "X", "O")
 
@@ -11,34 +20,51 @@ fun main(args: Array<String>) {
 
     printBoard()
     val winner = evaluate()
+
+
     for (i in gameSituation) {
 
         if (i == "X" || i == "O") {
-            gameField = "X"
-            gameField = "O"
+            positive.add(i)
         } else {
-            gameField = null
+            negative.add(i)
         }
     }
 
-
-
-    println("ist es eine gültige Spielsituation ?")
-    isValid(gameField)
+    println("Ist es eine gültige Spielfelden ?")
+    isValid(negative)
     println("------------------------------------------")
     println(winner)
+    println("------------------------------------------")
+
 
 }
 
-fun isValid(field: String?): Boolean {
+/**
+ * isValid method to check if the input are right.
+ *
+ * @param negative to get all the inputs that's not right.
+ * @return true when the input is right.
+ */
+fun isValid(negative: ArrayList<String>): Boolean? {
+    var isValid: Boolean? = null
 
-    val isValid: Boolean = field == "X" || field == "O"
-    println(isValid)
+    if (negative.isEmpty()) {
+        println("ja, gültige Spielfelden")
+        isValid = true
+    } else if (negative.isNotEmpty()) {
+        println("Nein, ungültige Spielfelden bitte geben Sie Nur X oder O ein")
+        isValid = false
 
+    }
     return isValid
 }
 
-
+/**
+ * evaluate method to check the Winner.
+ *
+ * @return the Result of the Game as Text.
+ */
 private fun evaluate(): String? {
     for (a in 0..7) {
         var line: String? = null
@@ -53,19 +79,23 @@ private fun evaluate(): String? {
             7 -> line = gameSituation[2] + gameSituation[4] + gameSituation[6]
         }
         if (line == "XXX") {
-            return "Der Spieler (X) hat gewonnen."
+            return "*.Der Spieler (X) hat gewonnen.*"
         } else if (line == "OOO") {
-            return "Der Spieler (O) hat gewonnen."
+            return "*.Der Spieler (O) hat gewonnen.*"
         }
     }
     for (a in 0..8) {
         if (Arrays.asList<String>(*board).contains((a + 1).toString())) {
             break
-        } else if (a == 8) return "Das Spiel endete mit einem Unentschieden."
+        } else if (a == 8) return "*.Das Spiel endete mit einem Unentschieden.*"
     }
+    print("*.Das Spiel ist noch nicht entschieden.*")
     return null
 }
 
+/**
+ * printBoard method to print Board around the input.
+ */
 private fun printBoard() {
     println("/---|---|---\\")
     println("| " + gameSituation[0] + " | " + gameSituation[1] + " | " + gameSituation[2] + " |")
@@ -75,20 +105,3 @@ private fun printBoard() {
     println("| " + gameSituation[6] + " | " + gameSituation[7] + " | " + gameSituation[8] + " |")
     println("/---|---|---\\")
 }
-
-
-/*
-  val isNotValid: Boolean = isItOk.isEmpty()
-    val text: String
-
-    text = if (isNotValid) {
-        "ungültige"
-    } else {
-        "gültige"
-    }
-    println("es ist ein $text Spielsituation, spieler $Player hat(${isItOk.size} mal $Player)eingegeben")
-
-
-    println(!isNotValid)
-    return !isNotValid
- */
